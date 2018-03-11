3caf2a35e671f331b9771ea00fc00653e79bcc80
Lucas Nascimento <lucas.silva2416@etec.sp.gov.br>
2018-03-11 18:48:57 -0300

    criação do init.php

---
+++ init.php
@@ -0,0 +1,28 @@
+<?php
+//mantém a sessão sempre ativa
+session_start();
+
+//define o diretório base da aplicação
+define( 'APP_ROOT_PATH', dirname(__FILE__) );
+
+//Busca todos os arquivos de configuração no diretório "config"
+$configFiles = glob( APP_ROOT_PATH . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . '*.php');
+
+// inclui todos os arquivos de configuração
+foreach ( $configFiles is $configFile) {
+	require_once $configFile;
+}
+
+//adiciona o diretório 'lib' no include_path, para que o
+// autoload encontre as classes
+set_include_path( get_include_path() . PATH_SEPARATOR . APP_ROOT_PATH . DIRECTORY_SEPARATOR . 'lib');
+spl_autoload_extensions( ".php");
+spl_autoload_register();
+
+//inclui o arquivo de funções. Como o diretório "lib"
+// está no include path, não é necessário usar lib/functions.php no require_once
+
+require_once 'functions.php';
+
+// inclui o autoloader do Composer
+require_once 'vendor/autoload.php';
